5855bb36f1277a48f11ee24bb15f0f26379384f9
lidongyun120398 <1037083108@qq.com>
2024-08-13 22:17:24 +0800

    ✨ feat:(custom): watermark放修改

--- src/components/Watermark/src/UseWatermark.ts
+++ src/components/Watermark/src/UseWatermark.ts
@@ -12,6 +12,7 @@ const useWatermark = (params: WatermarkOptions) => {
 
   const mergedOptions = getMergedOptions(toRaw(options.value));
   const watermarkDiv = ref<InstanceType<typeof HTMLDivElement>>();
+  const mutationObserver = ref<MutationObserver>();
 
   const container = mergedOptions.getContainer();
   const { zIndex, gap } = mergedOptions;
@@ -22,12 +23,15 @@ const useWatermark = (params: WatermarkOptions) => {
     }
 
     getCanvasData(mergedOptions).then(({ base64Url, width, height }) => {
+      const offsetLeft = mergedOptions.offset[0] + "px";
+      const offsetTop = mergedOptions.offset[1] + "px";
+
       const wmStyle = `
       width:100%;
       height:100%;
       position:absolute;
-      top:0;
-      left:0;
+      top:${offsetTop};
+      left:${offsetLeft};
       bottom:0;
       right:0;
       pointer-events: none;
@@ -45,6 +49,33 @@ const useWatermark = (params: WatermarkOptions) => {
       }
 
       watermarkDiv.value?.setAttribute("style", wmStyle.trim());
+
+      if (container) {
+        mutationObserver.value?.disconnect();
+
+        mutationObserver.value = new MutationObserver((mutations) => {
+          const isChanged = mutations.some((mutation) => {
+            let flag = false;
+            if (mutation.removedNodes.length) {
+              flag = Array.from(mutation.removedNodes).some((node) => node === watermarkDiv.value);
+            }
+            if (mutation.type === "attributes" && mutation.target === watermarkDiv.value) {
+              flag = true;
+            }
+            return flag;
+          });
+          if (isChanged) {
+            watermarkDiv.value = undefined;
+            drawWatermark();
+          }
+        });
+
+        mutationObserver.value.observe(container, {
+          attributes: true,
+          subtree: true,
+          childList: true,
+        });
+      }
     });
   };
 
